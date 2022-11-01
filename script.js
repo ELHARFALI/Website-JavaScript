ec3be712bca8ad938fa20a3456985ccdb8f5645b
ELHARFALI <elharfali.youssef.20@gmail.com>
2022-11-01 17:05:16 +0100

    Add smoth scrolling

--- script.js
+++ script.js
@@ -37,3 +37,7 @@ document.addEventListener('keydown', function (e) {
 
 const btnScrollTo = document.querySelector('.btn--scroll-to')
 const section1 = document.querySelector('#section--1')
+
+btnScrollTo.addEventListener('click', function () {
+  section1.scrollIntoView({behavior: 'smooth'})
+})
